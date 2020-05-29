99b1cba43b5d2c39e37a1293bd4df7c183f17d67
katia-khomtsova <66143453+katia-khomtsova@users.noreply.github.com>
2020-05-30 01:13:57 +0300

    Add files via upload

---
+++ js/slick-init.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $('.slider').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        pauseOnDotsHover: true,
+        slidesToShow: 1,
+        speed: 350
+    }).on("mousewheel", function (event) {
+        event.preventDefault();
+        if (event.deltaX > 0 || event.deltaY < 0) {
+            $(this).slick('slickNext');
+        } else if (event.deltaX < 0 || event.deltaY > 0) {
+            $(this).slick('slickPrev');
+        };
+        event.stopPropagation();
+    }).on('mousedown touchstart', function () {
+    }).on('mousemove touchmove', function () {
+        event.stopPropagation();
+    }).on('mouseleave touchend mouseup', function () {
+    });
+});
